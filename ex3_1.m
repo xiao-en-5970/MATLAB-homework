e9ef48079f1593ee17fd5c41d59fa204bd432b2f
xiao_en <2022210826@mail.hfut.edu.cn>
2024-06-04 19:40:38 +0800

    update

--- ex3_1.m
+++ ex3_1.m
@@ -1,4 +1,5 @@
 %% 数据预处理
+clc,clear;
 data = readtable("data_3_1.csv");
 r = data.r;
 q = data.q;
@@ -6,6 +7,22 @@ p = data.p;
 u = data.u;
 n = data.n;
 r0 = data.r0;
+N=15;
 
-%% 计算
-% 略，不会算
+%% 计算​​​​
+a=0; 
+hold on;
+while a<0.2   
+    c=[-0.05,-(data.r-data.p)'/100];     
+    A=[zeros(N,1),diag((data.q/100)')];     
+    b=a*ones(N,1);     
+    Aeq=[1,(data.p/100+1)'];     
+    beq=1;     
+    LB=zeros(N+1,1);     
+    [x,Q]=linprog(c,A,b,Aeq,beq,LB);     
+    Q=-Q;     
+    plot(a,Q,'*r');     
+    a=a+0.001; 
+end 
+xlabel('a(风险)'),ylabel('Q(收益)') 
+fprintf("风险越高，收益越大，但是在图中，风险a接近0.05的时候，风险增加，收益提高不明显\n")
